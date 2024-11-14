5d69a087c3ff0916afe426b409c050a15436ff0b
luinnaaa <00001108949940sp@al.educacao.sp.gov.br>
2024-11-13 22:11:34 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Musculação',
-    'Quem ganhou o Mr. Olympia 2024?',
-    'O Python é uma linguagem de programação'
+    'HIP HOP',
+    'Em qual ano as gêmeas Tasha e Tracie lançaram sua primeira música?',
+    'A primeira álbum lançado foi e "Rouff" com parceria da Ashira em 2019.
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Referências',
+    'Em "Tang" feat. Kyan, em um trecho da música aparece a letra de uma outra música de outro artista. Qual é esse trecho e quem é o artista?',
+    '"Essas são as meninas, que os meninos gosta" é um trecho da música da MC Menorzinha que leva o nome da frase.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Kyan',
+    'Qual é a frase a que a Tasha canta na música "Tipo, sonho de consumo!" do Kyan?',
+    'A frase é "Tipo sonho de consumo".'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Álbum',
+    'A música "Desce Licor" é de qual álbum da Tasha e Traice?',
+    ' "Desce Licor" é do álbum Ying Yang com a participação de Kyan e Rapper Gregory'
 )
